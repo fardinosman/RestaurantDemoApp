6b4ef3bb285c65f3243b6861ab01406b8acbb8b6
Fardin Osman <osmanfardin@hotmail.dk>
2021-03-12 10:40:17 +0100

    Controller added with Tuple

---
+++ RestaurantDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using RestaurantDemoApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            CustomerRepository objCustomerRepository = new CustomerRepository();
+            ItemRepository objItemRepository = new ItemRepository();
+            PaymentRepositroy objPaymentRepositroy = new PaymentRepositroy();
+           
+            var objMultipleModels =                  
+                new Tuple<IEnumerable<SelectListItem>,
+                 IEnumerable<SelectListItem>, 
+                 IEnumerable<SelectListItem>>(objCustomerRepository.GetAllCustomers(),
+                 objItemRepository.GetAllItems(),
+                 objPaymentRepositroy.GetAllPaymentType());
+            
+            
+            return View(objMultipleModels);
+        }
+    }
+}
